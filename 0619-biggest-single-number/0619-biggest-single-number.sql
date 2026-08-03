3cf938480e886cd15abfba6fda25c2c85e84467e
Akash Singh <akashsinghas2802@gmail.com>
2026-08-03 19:45:23 +0530

    Time: 582 ms (10.94%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,8 +1,13 @@
 # Write your MySQL query statement below
 select max(num) as num
+-- subquerry likhenge
 from(
     select num
     from MyNumbers
     group by num
     having count(*)=1
 ) as n;
+
+
+
+-- n count as temporary 
