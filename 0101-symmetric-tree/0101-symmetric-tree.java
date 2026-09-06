a032280de527853f2e9a8fa547048c2d7485c787
Akash Singh <akashsinghas2802@gmail.com>
2026-09-06 11:57:22 +0530

    Time: 0 ms (100%), Space: 43.3 MB (93.33%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -13,28 +13,15 @@
  *     }
  * }
  */
-
 class Solution {
-
     public boolean isSymmetric(TreeNode root) {
         return isMirror(root.left, root.right);
     }
+    boolean isMirror(TreeNode a, TreeNode b){
+        if(a==null && b==null) return true;
+        if(a==null || b==null) return false;
+        if(a.val != b.val)return false;
 
-    boolean isMirror(TreeNode a, TreeNode b) {
-
-        if (a == null && b == null) {
-            return true;
-        }
-
-        if (a == null || b == null) {
-            return false;
-        }
-
-        if (a.val != b.val) {
-            return false;
-        }
-
-        return isMirror(a.left, b.right)
-            && isMirror(a.right, b.left);
+        return isMirror(a.left, b.right) && isMirror(a.right, b.left);  
     }
 }
